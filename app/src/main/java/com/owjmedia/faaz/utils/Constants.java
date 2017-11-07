ae92572da80a1c67b0b12ac5db76cf97916bcdbb
Unknown <salman.seifian@gmail.com>
2017-11-07 20:31:29 +0330

    new: add font

---
+++ app/src/main/java/com/owjmedia/faaz/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.owjmedia.faaz.utils;
+
+
+
+public class Constants {
+
+    public static final class FONTS{
+
+        public static final String FONT_NAME_BOLD = "IRANYekanMobileBold.ttf";
+        public static final String FONT_NAME_REGULAR = "IRANYekanMobileRegular.ttf";
+        public static final String FONT_PATH = "fonts/";
+    }
+}
--- app/src/main/java/com/owjmedia/faaz/utils/CustomTextView.java
+++ app/src/main/java/com/owjmedia/faaz/utils/CustomTextView.java
@@ -6,32 +6,32 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-/**
- * Created by salman on 11/6/17.
- */
 
 public class CustomTextView extends TextView {
     public CustomTextView(Context context) {
         super(context);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(),"IRANSans.ttf");
-        this.setTypeface(typeface);
+        setTypeface(context);
     }
 
     public CustomTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(),"IRANSans.ttf");
-        this.setTypeface(typeface);
+        setTypeface(context);
     }
 
     public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(),"IRANSans.ttf");
-        this.setTypeface(typeface);
+        setTypeface(context);
     }
 
     public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(),"IRANSans.ttf");
-        this.setTypeface(typeface);
+        setTypeface(context);
+    }
+
+    private void setTypeface(Context context) {
+        Typeface regularTypeface = Typeface.createFromAsset(context.getAssets(), Constants.FONTS.FONT_PATH + Constants.FONTS.FONT_NAME_REGULAR);
+        Typeface boldTypeface = Typeface.createFromAsset(context.getAssets(), Constants.FONTS.FONT_PATH + Constants.FONTS.FONT_NAME_BOLD);
+        this.setTypeface(regularTypeface, Typeface.NORMAL);
+        this.setTypeface(boldTypeface, Typeface.BOLD);
     }
 }
