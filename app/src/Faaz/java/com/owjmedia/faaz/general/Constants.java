c490a33130f56b3156395793ed36e9e38774a22f
Unknown <salman.seifian@gmail.com>
2017-11-18 15:10:00 +0330

    update: change background colors

---
+++ app/src/Faaz/java/com/owjmedia/faaz/general/Constants.java
@@ -0,0 +1,21 @@
+package com.owjmedia.faaz.general;
+
+
+public class Constants {
+
+    public static final String BASE_URL = "http://82.102.10.148:8002/";
+    public static final String PREFS_NAME = "prefs_name";
+
+    public static final class KEYS {
+        public static final String TOKEN = "token";
+        public static final String EXPIRES_IN = "expires_in";
+        public static final String POLL_ID = "pollId";
+        public static final String NEWS_ID = "newsId";
+    }
+
+    public static final class FONTS {
+        public static final String FONT_NAME_BOLD = "IRANYekanMobileBold.ttf";
+        public static final String FONT_NAME_REGULAR = "IRANYekanMobileRegular.ttf";
+        public static final String FONT_PATH = "fonts/";
+    }
+}
